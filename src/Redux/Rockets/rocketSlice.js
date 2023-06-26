8eed357e060f6d7204666ba80986a08dd6433f86
Ankit <ankit26kushwa@gmail.comm>
2023-06-26 21:11:05 +0530

    create redux store

--- src/Redux/Rockets/rocketSlice.js
+++ src/Redux/Rockets/rocketSlice.js
@@ -1,6 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+
 const rocketSlice = createSlice({
-  name: "rockets",
+  name: 'rockets',
   initialState: [],
   reducers: {
     addRocket: () => {},
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,1 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rocketSlice from './Rockets/rocketSlice';
+
+const store = configureStore({
+  reducer: {
+    Rockets: rocketSlice,
+  },
+});
+
+export default store;
