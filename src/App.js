9df1ae16b664ad28d661ca514e4c2652998379b9
Ankit <ankit26kushwa@gmail.comm>
2023-06-27 10:55:01 +0530

    Add dispatch fetchdata in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import NavLink from './components/NavLink';
 import Profile from './components/Profile';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 
+import { fetchdata } from './Redux/Rockets/rocketSlice';
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchdata());
+  }, [dispatch]);
   return (
     <>
       <NavLink />
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,12 +1,4 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchdata } from '../Redux/Rockets/rocketSlice';
-
 function Rockets() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchdata());
-  }, [dispatch]);
   return <h1>Rockets Page</h1>;
 }
 
