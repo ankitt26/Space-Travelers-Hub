d07285a6762656f7b06d45f17dc8baeae17420d5
Ankit <ankit26kushwa@gmail.comm>
2023-06-27 14:37:58 +0530

    Add style in rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,26 +1,47 @@
-import { useSelector } from 'react-redux';
+import styles from '../CSS/Rockets.module.css';
 
-const Rockets = () => {
-  const data = useSelector((state) => state.Rockets.rocketData);
-  console.log(data);
-  return (
-    <main>
+const Rockets = () => (
+  <>
+    <main className={styles.rocket}>
       <img
+        className={styles.image}
         src="https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg"
         alt="space"
       />
-      <div>
-        <h2>Falco 1</h2>
-        <p>
+      <div className={styles.details}>
+        <h2 className={styles.name}>Falco 1</h2>
+        <p className={styles.description}>
           The Falcon 1 was an expendable launch system privately developed and
           manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon
           1 became the first privately-developed liquid-fuel launch vehicle to
           go into orbit around the Earth.
         </p>
-        <button type="submit">Reserve Rocket </button>
+        <button className={styles.button} type="submit">
+          Reserve Rocket
+          {' '}
+        </button>
       </div>
     </main>
-  );
-};
-
+    <main className={styles.rocket}>
+      <img
+        className={styles.image}
+        src="https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg"
+        alt="space"
+      />
+      <div className={styles.details}>
+        <h2 className={styles.name}>Falco 1</h2>
+        <p className={styles.description}>
+          The Falcon 1 was an expendable launch system privately developed and
+          manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon
+          1 became the first privately-developed liquid-fuel launch vehicle to
+          go into orbit around the Earth.
+        </p>
+        <button className={styles.button} type="submit">
+          Reserve Rocket
+          {' '}
+        </button>
+      </div>
+    </main>
+  </>
+);
 export default Rockets;
