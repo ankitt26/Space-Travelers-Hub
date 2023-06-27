1d7d8cb14de965a8ab6ea90fd7494dc4d97379d6
Ankit <ankit26kushwa@gmail.comm>
2023-06-27 15:10:14 +0530

    Render rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,47 +1,20 @@
+import { useSelector } from 'react-redux';
 import styles from '../CSS/Rockets.module.css';
 
-const Rockets = () => (
-  <>
-    <main className={styles.rocket}>
-      <img
-        className={styles.image}
-        src="https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg"
-        alt="space"
-      />
+const Rockets = () => {
+  const data = useSelector((state) => state.Rockets.rocketData);
+  const display = data.map((rocket) => (
+    <main className={styles.rocket} key={rocket.id}>
+      <img className={styles.image} src={rocket.flickr_images} alt="space" />
       <div className={styles.details}>
-        <h2 className={styles.name}>Falco 1</h2>
-        <p className={styles.description}>
-          The Falcon 1 was an expendable launch system privately developed and
-          manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon
-          1 became the first privately-developed liquid-fuel launch vehicle to
-          go into orbit around the Earth.
-        </p>
+        <h2 className={styles.name}>{rocket.name}</h2>
+        <p className={styles.description}>{rocket.description}</p>
         <button className={styles.button} type="submit">
           Reserve Rocket
-          {' '}
         </button>
       </div>
     </main>
-    <main className={styles.rocket}>
-      <img
-        className={styles.image}
-        src="https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg"
-        alt="space"
-      />
-      <div className={styles.details}>
-        <h2 className={styles.name}>Falco 1</h2>
-        <p className={styles.description}>
-          The Falcon 1 was an expendable launch system privately developed and
-          manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon
-          1 became the first privately-developed liquid-fuel launch vehicle to
-          go into orbit around the Earth.
-        </p>
-        <button className={styles.button} type="submit">
-          Reserve Rocket
-          {' '}
-        </button>
-      </div>
-    </main>
-  </>
-);
+  ));
+  return <section className={styles.rocket_page}>{display}</section>;
+};
 export default Rockets;
