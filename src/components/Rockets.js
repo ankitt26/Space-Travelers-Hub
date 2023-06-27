486630c0378194193c7760fc3a2df06dad976849
Ankit <ankit26kushwa@gmail.comm>
2023-06-27 10:44:23 +0530

    dispatch fetchdata and store in redux store

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchdata } from '../Redux/Rockets/rocketSlice';
+
 function Rockets() {
-  return (
-    <h1>Rockets Page</h1>
-  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchdata());
+  }, [dispatch]);
+  return <h1>Rockets Page</h1>;
 }
 
 export default Rockets;
