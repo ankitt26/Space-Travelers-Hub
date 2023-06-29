af2faf6dd29ac8a0ad2fdae25fa50e9acc4d54ab
Ankit <ankit26kushwa@gmail.comm>
2023-06-29 12:21:42 +0530

    filter Reserved rockets

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,27 +1,34 @@
-const MyProfile = () => (
-  <main className="Myprofile">
-    <div>
-      <h2> My Mission</h2>
-      <div className="reserved_missions">
-        <h3>Mission 1</h3>
-        <h3>Mission 1</h3>
-        <h3>Mission 1</h3>
-      </div>
-    </div>
-    {/* Rocket section */}
-    <div>
-      <h2>My Rockets</h2>
-      <ProfileRocket />
-    </div>
-  </main>
-);
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-const ProfileRocket = () => (
-  <div className="reserved_rockets">
-    <h3>Falco 9</h3>
-    <h3>Falco 9</h3>
-    <h3>Falco 9</h3>
-  </div>
-);
+const MyProfile = () => {
+  const data = useSelector((state) => state.Rockets.rocketData);
+  const Myrocket = data.filter((element) => element.reserved === true);
+  const renderRocket = Myrocket.map((element) => (
+    <h2 key={element.id} id={element.id}>
+      {' '}
+      {element.name}
+    </h2>
+  ));
+
+  return (
+    <main className="Myprofile">
+      {/* Mission Section */}
+      <div>
+        <h2> My Mission</h2>
+        <div className="reserved_missions">
+          <h3>Mission 1</h3>
+          <h3>Mission 1</h3>
+          <h3>Mission 1</h3>
+        </div>
+      </div>
+      {/* Rocket section */}
+      <div>
+        <h2>My Rockets</h2>
+        {renderRocket}
+      </div>
+    </main>
+  );
+};
 
 export default MyProfile;
