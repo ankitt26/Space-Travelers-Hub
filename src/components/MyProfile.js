eb3d75068fed7d734e6d56cd25bc5dea63842d9e
Ankit <ankit26kushwa@gmail.comm>
2023-06-29 13:30:12 +0530

    style myprofile section

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Styles from '../CSS/MyProfile.module.css';
 
 const MyProfile = () => {
   const data = useSelector((state) => state.Rockets.rocketData);
   const Myrocket = data.filter((element) => element.reserved === true);
   const renderRocket = Myrocket.map((element) => (
-    <h2 key={element.id} id={element.id}>
+    <h4 className={Styles.rockets} key={element.id} id={element.id}>
       {' '}
       {element.name}
-    </h2>
+    </h4>
   ));
 
   return (
-    <main className="Myprofile">
+    <main className={Styles.MyProfile}>
       {/* Mission Section */}
-      <div>
-        <h2> My Mission</h2>
-        <div className="reserved_missions">
-          <h3>Mission 1</h3>
-          <h3>Mission 1</h3>
-          <h3>Mission 1</h3>
+      <div className={Styles.mission_section}>
+        <h2 className={Styles.heading2}> My Mission</h2>
+        <div className={Styles.reserved}>
+          <h4 className={Styles.rockets}>Mission 1</h4>
+          <h4 className={Styles.rockets}>Mission 1</h4>
+          <h4 className={Styles.rockets}>Mission 1</h4>
         </div>
       </div>
       {/* Rocket section */}
-      <div>
-        <h2>My Rockets</h2>
-        {renderRocket}
+      <div className={Styles.rocket_section}>
+        <h2 className={Styles.heading2}>My Rockets</h2>
+        <div className={Styles.reserved}>{renderRocket}</div>
       </div>
     </main>
   );
